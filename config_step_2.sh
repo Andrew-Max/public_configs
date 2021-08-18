f7e6e68f9e7106df78c0d48c2c8e5a024a011700
Andrew <anm898989@gmail.com>
2021-08-18 18:22:26 +0200

    cleanup

--- config_step_2.sh
+++ config_step_2.sh
@@ -31,14 +31,17 @@ git config --global core.excludesfile ~/opt/gitignore_global
 #=======================================
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install sublime-text -y
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
+#=======================================
+# Install Curl
+#=======================================
+sudo apt install curl -y
 
 #=======================================
 # Set up RVM
 #=======================================
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-sudo apt install curl -y
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 \source ~/.rvm/scripts/rvm
 
