e27d30e11fbac1e7f654af73535813e89e1eb8d3
Andrew <anm898989@gmail.com>
2021-08-31 12:34:32 -0500

    whitespace in sublime install

--- config_step_2.sh
+++ config_step_2.sh
@@ -31,7 +31,7 @@ git config --global core.excludesfile ~/opt/gitignore_global
 #=======================================
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install sublime-text -y
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 #=======================================
 # Install Curl
