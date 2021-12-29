bf2e51fbdd4a31d4f0179a33081998b3cc5a5363
Andrew <anm898989@gmail.com>
2021-12-28 22:57:46 -0500

    remove old sublime install

--- config_step_2.sh
+++ config_step_2.sh
@@ -27,13 +27,6 @@ echo "**/application.yml" >> ~/opt/gitignore_global
 git config --global core.excludesfile ~/opt/gitignore_global
 
 #=======================================
-# Set up Sublime Text
-#=======================================
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install sublime-text -y
-echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-#=======================================
 # Install Curl
 #=======================================
 sudo apt install curl -y
